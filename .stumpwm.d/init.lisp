0c0f93deeabc4ba86e111ad05e1f81f7b649bd84
Mike Aldred <mike.aldred@luminousmonkey.org>
2020-01-26 15:37:58 +0800

    Updates the keymap a little

--- .stumpwm.d/init.lisp
+++ .stumpwm.d/init.lisp
@@ -12,6 +12,9 @@
       *suppress-abort-messages* t
       *shell-program* (getenv "SHELL"))
 
+;; Basics
+(set-prefix-key (kbd "s-t"))
+
 (set-module-dir
  (pathname-as-directory (concat (getenv "HOME")
                                 "/.stumpwm.d/stumpwm-contrib")))
@@ -78,13 +81,63 @@ run-or-raise with group search t."
 (define-key *root-map* (kbd "e") "exec ec")
 (define-key *root-map* (kbd "c") "exec kitty")
 
-(set-fg-color "#eee8d5")
-(set-bg-color "#586e75")
-(set-border-color "#fdf6e3")
+(setf *colors* (list "#073642"          ; 0 black
+                     "#dc322f"          ; 1 red
+                     "#859900"          ; 2 green
+                     "#b58900"          ; 3 yellow
+                     "#268bd2"          ; 4 blue
+                     "#d33682"          ; 5 magenta
+                     "#2aa198"          ; 6 cyan
+                     "#eee8d5"))        ; 7 white
+
+;;SOLARIZED HEX     16/8 TERMCOL  XTERM/HEX   L*A*B      RGB         HSB
+;;--------- ------- ---- -------  ----------- ---------- ----------- -----------
+;;base03    #002b36  8/4 brblack  234 #1c1c1c 15 -12 -12   0  43  54 193 100  21
+;;base02    #073642  0/4 black    235 #262626 20 -12 -12   7  54  66 192  90  26
+;;base01    #586e75 10/7 brgreen  240 #585858 45 -07 -07  88 110 117 194  25  46
+;;base00    #657b83 11/7 bryellow 241 #626262 50 -07 -07 101 123 131 195  23  51
+;;base0     #839496 12/6 brblue   244 #808080 60 -06 -03 131 148 150 186  13  59
+;;base1     #93a1a1 14/4 brcyan   245 #8a8a8a 65 -05 -02 147 161 161 180   9  63
+;;base2     #eee8d5  7/7 white    254 #e4e4e4 92 -00  10 238 232 213  44  11  93
+;;base3     #fdf6e3 15/7 brwhite  230 #ffffd7 97  00  10 253 246 227  44  10  99
+;;yellow    #b58900  3/3 yellow   136 #af8700 60  10  65 181 137   0  45 100  71
+;;orange    #cb4b16  9/3 brred    166 #d75f00 50  50  55 203  75  22  18  89  80
+;;red       #dc322f  1/1 red      160 #d70000 50  65  45 220  50  47   1  79  86
+;;magenta   #d33682  5/5 magenta  125 #af005f 50  65 -05 211  54 130 331  74  83
+;;violet    #6c71c4 13/5 brmagenta 61 #5f5faf 50  15 -45 108 113 196 237  45  77
+;;blue      #268bd2  4/4 blue      33 #0087ff 55 -10 -45  38 139 210 205  82  82
+;;cyan      #2aa198  6/6 cyan      37 #00afaf 60 -35 -05  42 161 152 175  74  63
+;;green     #859900  2/2 green     64 #5f8700 60 -20  65 133 153   0  68 100  60
+
+(set-fg-color (nth 0 *colors*))
+(set-bg-color (nth 7 *colors*))
+(set-border-color (nth 0 *colors*))
+(set-focus-color "#586e75")
+(set-unfocus-color "#073642")
+(set-win-bg-color (nth 0 *colors*))
+(set-float-focus-color "#586e75")
+(set-float-unfocus-color "#073642")
+
+(defun shift-windows-forward (frames win)
+  "Exchange windows through cycling frames."
+  (when frames
+    (let ((frame (car frames)))
+      (shift-windows-forward (cdr frames)
+                             (stumpwm::frame-window frame))
+      (when win
+        (stumpwm::pull-window win frame)))))
+
+(defcommand rotate-windows () ()
+  (let* ((frames (stumpwm::head-frames (current-group) (current-head)))
+         (win (stumpwm::frame-window (car (last frames)))))
+    (shift-windows-forward frames win)))
+
+(define-key *top-map* (kbd "s-r") "rotate-windows")
 
 ;; Init
 (update-color-map (current-screen))
 (run-shell-command "sh ~/.fehbg")
+(run-shell-command "xmodmap .Xmodmap")
 (run-shell-command "xset b off")
 
 ;; Programs
